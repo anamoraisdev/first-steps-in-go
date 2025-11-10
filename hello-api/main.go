75fce2ebc6a2ca03b61a4b9ba104c0b53da57a32
anamoraisdev <anamorais.dev@gmail.com>
2025-11-10 15:31:47 -0300

    creating a server that responds to GET

---
+++ hello-api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Olá, mundo!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Servidor rodando em http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}

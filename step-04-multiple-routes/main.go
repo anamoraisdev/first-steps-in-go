fad63888d7a709d9a3c24a40066fe4b4ee77d47e
anamoraisdev <anamorais.dev@gmail.com>
2025-11-25 11:13:02 -0300

    creating getUsers

--- step-04-multiple-routes/main.go
+++ step-04-multiple-routes/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type User struct {
 	ID      int     `json:"id"`
 	Name    string  `json:"name"`
@@ -17,4 +23,30 @@ type Order struct {
 	UserID   int     `json:"user_id"`
 	Products []int   `json:"products"`
 	Total    float64 `json:"total"`
+}
+
+var users = []User{
+	{ID: 1, Name: "Ana", Balance: 200},
+	{ID: 2, Name: "Jo", Balance: 500},
+}
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func main() {
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			getUsersHandler(w, r)
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	fmt.Println("Server running on http://localhost:8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Server error:", err)
+	}
 }

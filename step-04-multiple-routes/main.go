24530d2f95f881c5f6c55a1c3cb48544aa56930d
anamoraisdev <anamorais.dev@gmail.com>
2025-11-25 20:14:15 -0300

    creating deleteUser

--- step-04-multiple-routes/main.go
+++ step-04-multiple-routes/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
@@ -48,15 +49,35 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(u)
 }
 
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	idUserString := r.URL.Path[len("/users/"):]
+	idUser, err := strconv.Atoi(idUserString)
+
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, u := range users {
+		if u.ID == idUser {
+			deletedUser := users[i]
+			users = append(users[:i], users[i+1:]...)
+			json.NewEncoder(w).Encode(deletedUser)
+			return
+		}
+	}
+	http.Error(w, "User not found", http.StatusNotFound)
+}
+
 func main() {
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/users/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
 			getUsersHandler(w, r)
-
 		case http.MethodPost:
 			createUserHandler(w, r)
-
+		case http.MethodDelete:
+			deleteUserHandler(w, r)
 		default:
 			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		}

38aec8326d082a7459713aa2e6f3c1d0076e8e97
anamoraisdev <anamorais.dev@gmail.com>
2025-11-28 15:40:47 -0300

    create endpoint to create new tasks

--- step-05-database-tasks/handlers.go
+++ step-05-database-tasks/handlers.go
@@ -16,8 +16,41 @@ func ListTasks(db *sqlx.DB) http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(tasks)
+	}
+}
+
+func CreateTask(db *sqlx.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var input struct {
+			Title string `json:"title"`
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&input)
+		if err != nil {
+			http.Error(w, "invalid JSON", http.StatusBadRequest)
+			return
+		}
+		if input.Title == "" {
+			http.Error(w, "title is required", http.StatusBadRequest)
+			return
+		}
+
+		var newTask Task
+		query := `
+			INSERT INTO tasks (title)
+			VALUES ($1)
+			RETURNING id, title, completed, created_at;
+		`
+		err = db.Get(&newTask, query, input.Title)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newTask)
 	}
 }
--- step-05-database-tasks/main.go
+++ step-05-database-tasks/main.go
@@ -9,7 +9,15 @@ func main() {
 	db := ConnectDB()
 	defer db.Close()
 
-	http.HandleFunc("/tasks", ListTasks(db))
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			ListTasks(db)(w, r)
+		case http.MethodPost:
+			CreateTask(db)(w, r)
+		}
+	})
+
 	log.Println("Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

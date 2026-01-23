889e31460f8e5b1abfc7eea0253c189ad1f91e3c
anamoraisdev <anamorais.dev@gmail.com>
2026-01-23 13:04:51 -0300

    refactor: rename project and database setup to match new study activity

---
+++ step-07-relational-modeling/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"step-07-relational-modeling/cmd/internal/db"
+)
+
+func main() {
+	database := db.ConnectDB()
+	defer database.Close()
+
+	log.Println("🚀 Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

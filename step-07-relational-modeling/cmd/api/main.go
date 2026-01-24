20ed7ea8f29107324fd31b66d1da3063e25ee586
anamoraisdev <anamorais.dev@gmail.com>
2026-01-24 15:31:09 -0300

    config router

--- step-07-relational-modeling/cmd/api/main.go
+++ step-07-relational-modeling/cmd/api/main.go
@@ -4,31 +4,15 @@ import (
 	"log"
 	"net/http"
 	"step-07-relational-modeling/internal/db"
-	"step-07-relational-modeling/internal/handlers"
+	httpRouter "step-07-relational-modeling/internal/http"
 )
 
 func main() {
 	database := db.ConnectDB()
 	defer database.Close()
 
-	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			handlers.RegisterStudent(database)(w, r)
-		case http.MethodGet:
-			handlers.ListStudents(database)(w, r)
-		}
-	})
-
-	http.HandleFunc("/courses", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			handlers.CreateCourse(database)(w, r)
-		case http.MethodGet:
-			handlers.ListCourses(database)(w, r)
-		}
-	})
+	router := httpRouter.NewRouter(database)
 
 	log.Println("🚀 Server running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ step-07-relational-modeling/internal/http/router.go
@@ -0,0 +1,34 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jmoiron/sqlx"
+
+	"step-07-relational-modeling/internal/http/handlers"
+)
+
+func NewRouter(db *sqlx.DB) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	r.Route("/students", func(r chi.Router) {
+		r.Post("/", handlers.RegisterStudent(db))
+		r.Get("/", handlers.ListStudents(db))
+	})
+
+	r.Route("/courses", func(r chi.Router) {
+		r.Post("/", handlers.CreateCourse(db))
+		r.Get("/", handlers.ListCourses(db))
+
+		r.Route("/{course_id}", func(r chi.Router) {
+			r.Post("/lessons", handlers.CreateLesson(db))
+		})
+	})
+
+	return r
+}

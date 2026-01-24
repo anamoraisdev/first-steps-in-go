d9dbc26022ca61d3a22cde424c3ef4b955f66448
anamoraisdev <anamorais.dev@gmail.com>
2026-01-24 14:40:59 -0300

    create function to register students

--- step-07-relational-modeling/cmd/api/main.go
+++ step-07-relational-modeling/cmd/api/main.go
@@ -4,11 +4,19 @@ import (
 	"log"
 	"net/http"
 	"step-07-relational-modeling/internal/db"
+	"step-07-relational-modeling/internal/handlers"
 )
 
 func main() {
 	database := db.ConnectDB()
 	defer database.Close()
+
+	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			handlers.RegisterStudent(database)(w, r)
+		}
+	})
 
 	log.Println("🚀 Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
---
+++ step-07-relational-modeling/internal/handlers/students.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"step-07-relational-modeling/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func RegisterStudent(db *sqlx.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var newStudent models.Student
+
+		if err := json.NewDecoder(r.Body).Decode(&newStudent); err != nil {
+			http.Error(w, "invalid JSON body", http.StatusBadRequest)
+			return
+		}
+		if newStudent.Name == "" || newStudent.Email == "" {
+			http.Error(w, "name and email are required", http.StatusBadRequest)
+			return
+		}
+		query := `
+			INSERT INTO students (name, email)
+			VALUES ($1, $2)
+			RETURNING id, name, email, created_at;
+		`
+		err := db.Get(
+			&newStudent,
+			query,
+			newStudent.Name,
+			newStudent.Email,
+		)
+
+		if err != nil {
+			http.Error(w, "failed to create student", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newStudent)
+	}
+}
